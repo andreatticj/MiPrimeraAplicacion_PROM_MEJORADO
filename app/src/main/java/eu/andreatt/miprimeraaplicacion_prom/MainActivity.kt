01c5f3ac1aa093442c3f7963e681ce4c26f86f0a
andreatticj <andreatticj@gmail.com>
2024-10-24 22:34:36 +0200

    ADD libreria en build.gradle.kts (:app)

--- app/src/main/java/eu/andreatt/miprimeraaplicacion_prom/MainActivity.kt
+++ app/src/main/java/eu/andreatt/miprimeraaplicacion_prom/MainActivity.kt
@@ -1,20 +1,33 @@
 package eu.andreatt.miprimeraaplicacion_prom
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAddTask: Button
+    lateinit var etTask: EditText
+    lateinit var rvTask: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initUi()
+    }
+
+    private fun initUi() {
+        initView()
+    }
+
+    private fun initView() {
+        btnAddTask= findViewById(R.id.btnAddTask)
+        etTask= findViewById(R.id.etTask)
+        rvATask= findViewById(R.id.rvTask)
     }
 }
