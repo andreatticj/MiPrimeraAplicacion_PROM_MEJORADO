9512cd4554cac411259bb3d59f3715808ae98031
andreatticj <andreatticj@gmail.com>
2024-10-24 22:55:52 +0200

    ADD viewHolder

---
+++ app/src/main/java/eu/andreatt/miprimeraaplicacion_prom/TaskViewHolder.kt
@@ -0,0 +1,14 @@
+package eu.andreatt.miprimeraaplicacion_prom
+
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskViewHolder(view:View):RecyclerView.ViewHolder(view) {
+    private  val tvTask: TextView = view.findViewById(R.id.tvTask)
+
+    fun render(task:String){
+        tvTask.text=task
+    }
+}
